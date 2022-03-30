321b8a78e484d4aa146c3b37ebe3dcfd3e36052c
67W <hulimeno@yandex.ru>
2022-03-31 02:51:29 +0300

    Создан для задания 5 По желанию создайте дополнительный класс Channel для описания канала.

---
+++ src/main/kotlin/module7/practice/ClassChannel.kt
@@ -0,0 +1,23 @@
+package module7.practice
+
+class ClassChannel {
+    val nameList = mutableListOf(
+        "BBC One",
+        "BBC Two",
+        "BBC Scotland",
+        "BBC Three",
+        "BBC Four",
+        "Discovery",
+        "Animal Planet",
+        "BBC News",
+        "BBC Parliament",
+        "TLC",
+        "Channel 4",
+        "Channel 5",
+        "MTV",
+        "Nickelodeon",
+        "Comedy Central",
+        "Eurosport 1",
+        "National Geographic"
+    )
+}
