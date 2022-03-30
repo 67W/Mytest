1262762565399a15d0c2549caed3bf0e3e996037
67W <hulimeno@yandex.ru>
2022-03-31 02:52:32 +0300

    Доделал перемешивание каналов в объекте, но сделал возможным повторение названия канала в списке.

--- src/main/kotlin/module7/practice/Channels.kt
+++ src/main/kotlin/module7/practice/Channels.kt
@@ -1,4 +1,23 @@
 package module7.practice
 
+import kotlin.random.Random
+
 object Channels {
+    val countChannel = Random.nextInt(1, 10)
+    private val list = mutableListOf<Int>()
+    fun listChannels(input: Int): MutableList<Int> {
+        for (i in 1..input) {
+            list += i
+        }
+        return list
+    }
+
+    val nameList = mutableListOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "J", "k", "l", "m")
+    fun getRandomChannels(): MutableMap<Int, String> {
+        val mutMap = mutableMapOf<Int, String>()
+        for (i in list.indices) {
+            mutMap += Pair(list[i], nameList.shuffled()[i])
+        }
+        return mutMap
+    }
 }
