8287faea7417f2aad672256e77398f60db77f544
67W <hulimeno@yandex.ru>
2022-04-02 04:45:20 +0300

    новый экземпляр базового программиста developer

--- src/main/kotlin/module8/lesson3/main.kt
+++ src/main/kotlin/module8/lesson3/main.kt
@@ -4,6 +4,7 @@ fun main() {
     val ktDev = KotlinDeveloper("Alex", 25)
     val javaDev = JavaDeveloper(name = "Ivan", age = 40, experience = 20)
     val cppDev = CppDeveloper(name = "Anna", age = 30, experience = 7)
+    val  developer = Developer(name = "MegaMozg", age = 100, experience = 100)
 
     ktDev.writeCode()
     println("ktDev level - ${ktDev.getLevel()}")
@@ -16,4 +17,8 @@ fun main() {
     cppDev.writeCode()
     println("cppDev level - ${cppDev.getLevel()}")
     println("experience - ${cppDev.experience}")
+
+    developer.writeCode()
+    println("developer level - ${developer.getLevel()}")
+    println("experience - ${developer.experience}")
 }
