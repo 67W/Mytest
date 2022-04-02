842593b4aa3ba6ff3957edc24f3850040643333e
67W <hulimeno@yandex.ru>
2022-04-02 05:28:58 +0300

    prepare

--- src/main/kotlin/module8/practice/Animal.kt
+++ src/main/kotlin/module8/practice/Animal.kt
@@ -1,4 +1,18 @@
 package module8.practice
 
-class Animal {
+open class Animal(
+    val energy: Int,
+    val weight: Int,
+    val currentAge: Int,
+    val maxAge: Int,
+    val name: String
+) {
+    open fun isTooOld() = when {
+        currentAge >= maxAge -> true
+        else -> false
+    }
+
+    open fun sleep() = println("sleeping")
+    open fun eat() = println("eating")
+    open fun move() = println("moving")
 }
