79861c4143028540f17bffcf269e47ecd1983fe8
67W <hulimeno@yandex.ru>
2022-04-14 21:55:17 +0300

    anonymous

---
+++ src/main/kotlin/module10/lesson3/main.kt
@@ -0,0 +1,19 @@
+package module10.lesson3
+
+import module10.lesson1.Callable
+
+fun main() {
+    val anonymous = object :Callable{
+        val name = "anonymous"
+        val lastName = "object"
+        fun method() = println("anonymous object")
+        override val vendor = "anonymous object"
+
+        override fun call(number: String) {
+            println("anonymous object calling")
+        }
+    }
+    anonymous.name
+    anonymous.lastName
+    anonymous.method()
+}
