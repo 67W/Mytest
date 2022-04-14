08904c4f53deb3280a83ef9825ddd5856a294662
67W <hulimeno@yandex.ru>
2022-04-14 21:22:21 +0300

    play video add mobile phone ex

--- src/main/kotlin/module10/lesson1/main.kt
+++ src/main/kotlin/module10/lesson1/main.kt
@@ -3,4 +3,8 @@ package module10.lesson1
 fun main() {
     val devices = listOf(MobilePhone(), AppleWatch(), DiskPhone(), SmartFridge())
     devices.forEach { it.call("89219999999") }
+
+    val mobilePhone = MobilePhone()
+    mobilePhone.call("98745645656")
+    mobilePhone.playVideo("www.example.com")
 }
