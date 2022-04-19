1a7603790274f72e5a2165cebebdd1786ad05190
67W <hulimeno@yandex.ru>
2022-04-19 20:59:35 +0300

    Класс Any и сравнение объектов '===' это сравнение объектов по ссылке

---
+++ src/main/kotlin/module11/lesson1/main.kt
@@ -0,0 +1,12 @@
+package module11.lesson1
+
+import module7.lesson1.Car
+
+fun main() {
+    val car1 = Car()
+    val car2 = Car()
+    val car3 = car1
+
+    println(car1 === car3)
+    println(car1 === car2)
+}
