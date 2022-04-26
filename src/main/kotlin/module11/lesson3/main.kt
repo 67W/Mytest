626421d8c9ecf0f80927a6b21d91745be6ff2c4a
67W <hulimeno@yandex.ru>
2022-04-26 05:22:50 +0300

    Data-класс

--- src/main/kotlin/module11/lesson3/main.kt
+++ src/main/kotlin/module11/lesson3/main.kt
@@ -1,10 +1,13 @@
 package module11.lesson3
 
 fun main() {
-    val person = Person("name", "last name")
+    val map = mutableMapOf(
+        1 to "1",
+        2 to "2",
+        3 to "3"
+    )
 
-//    val (name, lastName) = person
-
-    val name = person.component1()
-    val lastName = person.component2()
+    for ((key,value) in map) {
+        println("$key - $value")
+    }
 }
