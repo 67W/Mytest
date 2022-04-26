34265ad2d178778ad8ede017896735880d713627
67W <hulimeno@yandex.ru>
2022-04-26 05:42:51 +0300

    Класс Enum обращение к предопределенным полям 'name'(это имя объекта WHITE RED и тд) и ordinal(это порядковый номер объекта)

--- src/main/kotlin/module11/lesson3/main.kt
+++ src/main/kotlin/module11/lesson3/main.kt
@@ -1,11 +1,7 @@
 package module11.lesson3
 
 fun main() {
-    Color.WHITE
-    println(Color.WHITE.hex)
-    Color.WHITE.draw()
-    Color.RED.draw()
-
-    println(Color.values().joinToString(","))
-    println(Color.valueOf("RED").hex)
+Color.values().forEach {
+    println("${it.name} - ${it.ordinal}")
+}
 }
