0b0a6fcc27993714662ad59ae1f1a057412c960d
67W <hulimeno@yandex.ru>
2022-04-26 05:47:40 +0300

    Класс Enum переназначение объектов если мы через when обрабатываем все возможные значения, то не надо указывать ветку else

--- src/main/kotlin/module11/lesson3/main.kt
+++ src/main/kotlin/module11/lesson3/main.kt
@@ -1,7 +1,16 @@
 package module11.lesson3
 
 fun main() {
-Color.values().forEach {
-    println("${it.name} - ${it.ordinal}")
+    Color.values().forEach {
+        println("${it.name} - ${it.ordinal}")
+    }
 }
-}
+
+fun switchColor(color: Color): Color =
+    when (color) {
+        Color.WHITE -> Color.BLACK
+        Color.BLACK -> Color.RED
+        Color.RED -> Color.GREEN
+        Color.GREEN -> Color.BLUE
+        Color.BLUE -> Color.BLACK
+    }
