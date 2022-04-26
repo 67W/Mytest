ab46171af767116dd80d1061ae7be375f265d6d6
67W <hulimeno@yandex.ru>
2022-04-26 04:45:58 +0300

    Параметрический полиморфизм на практике Практика

--- src/main/kotlin/module10/lesson4/main.kt
+++ src/main/kotlin/module10/lesson4/main.kt
@@ -3,12 +3,6 @@ package module10.lesson4
 import kotlin.random.Random
 
 fun main() {
-    val train = Train(10000)
-    println("capacity - ${train.capacity}")
-    train.move()
-    println("-----------")
-    println()
-
     val boeing737 = Boeing737()
     fillAircraft(boeing737)
     println(boeing737.getInfo())
@@ -19,35 +13,11 @@ fun main() {
     println("-----------------------")
     println()
 
-    val zeppelin = createAndFillZeppelin()
+    val zeppelin = Zeppelin()
+    fillAircraft(zeppelin)
     println(zeppelin.getInfo())
     zeppelin.getSeatScheme()
     printPassengerInfo(zeppelin, Seat(0, 'c'))
-}
-
-fun createAndFillBoeing(): Boeing737 {
-    val boeing737 = Boeing737()
-    val passengerCount = Random.nextInt(1, boeing737.capacity)
-    for (i in 0 until passengerCount) {
-        val seat = boeing737.getAvailableSeat() ?: return boeing737
-
-        val passenger = Passenger(
-            name = "Ivan",
-            lastName = "Petrov",
-            document = ForeignPassport(
-                series = Random.nextInt(1000, 9999).toString(),
-                number = Random.nextInt(100000, 999999).toString()
-            ),
-            seat = seat
-        )
-        boeing737.addPassenger(passenger)
-    }
-    return boeing737
-}
-
-fun createAndFillZeppelin(): Zeppelin {
-    val zeppelin = Zeppelin()
-    return zeppelin
 }
 
 fun printPassengerInfo(aircraft: Aircraft, seat: Seat) {
@@ -61,7 +31,6 @@ fun fillAircraft(aircraft: Aircraft) {
     val zeppelinPassengers = Random.nextInt(1, aircraft.capacity)
     for (i in 0 until zeppelinPassengers) {
         val seat = aircraft.getAvailableSeat() ?: return
-
         val passenger = Passenger(
             name = "Alex",
             lastName = "Ivanov",
@@ -72,7 +41,7 @@ fun fillAircraft(aircraft: Aircraft) {
     }
 }
 
-fun getRandomDocument(): Document {
+fun getRandomDocument(): Document =
     when (Random.nextInt(0, 2)) {
         0 -> ForeignPassport(
             series = Random.nextInt(1000, 9999).toString(),
@@ -83,5 +52,3 @@ fun getRandomDocument(): Document {
             issueDate = "01.01.1970"
         )
     }
-
-}
