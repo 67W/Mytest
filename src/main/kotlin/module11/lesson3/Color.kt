2b22ebfe97ed6822034bbc0c828cc724d29f617a
67W <hulimeno@yandex.ru>
2022-04-26 05:34:16 +0300

    Класс Enum переопределине функций как в стеле так и в перечислении енум класса

--- src/main/kotlin/module11/lesson3/Color.kt
+++ src/main/kotlin/module11/lesson3/Color.kt
@@ -1,13 +1,21 @@
 package module11.lesson3
 
-enum class Color(val hex: String) {
+enum class Color(val hex: String) : Drawble {
     WHITE("#FFF"),
     BLACK("#000"),
-    RED("#F00"),
+    RED("#F00") {
+        override fun draw() {
+            println("Overridden draw fun")
+        }
+    },
     GREEN("#0F0"),
     BLUE("#00F");
 
-    fun someMethod(){
+    fun someMethod() {
         println("print something")
+    }
+
+    override fun draw() {
+        println("draw color $hex")
     }
 }
---
+++ src/main/kotlin/module11/lesson3/Drawble.kt
@@ -0,0 +1,5 @@
+package module11.lesson3
+
+interface Drawble {
+    fun draw()
+}
