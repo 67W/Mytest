f97bff44c61bfd2298d93078fc977ffc934c3102
67W <hulimeno@yandex.ru>
2022-04-26 04:39:40 +0300

    Параметрический полиморфизм на практике Практика

--- src/main/kotlin/module10/lesson4/DeputyID.kt
+++ src/main/kotlin/module10/lesson4/DeputyID.kt
@@ -1,7 +1,9 @@
 package module10.lesson4
 
 class DeputyID(
-    private val number: String
+    private val number: String,
+    private val issueDate: String
+
 ) : Document {
     override fun getStringIdentifier(): String = number
 
--- src/main/kotlin/module10/lesson4/main.kt
+++ src/main/kotlin/module10/lesson4/main.kt
@@ -9,7 +9,8 @@ fun main() {
     println("-----------")
     println()
 
-    val boeing737 = createAndFillBoeing()
+    val boeing737 = Boeing737()
+    fillAircraft(boeing737)
     println(boeing737.getInfo())
     boeing737.getSeatScheme()
     printPassengerInfo(boeing737, Seat(0, 'c'))
@@ -46,20 +47,6 @@ fun createAndFillBoeing(): Boeing737 {
 
 fun createAndFillZeppelin(): Zeppelin {
     val zeppelin = Zeppelin()
-    val zeppelinPassengers = Random.nextInt(1, zeppelin.capacity)
-    for (i in 0 until zeppelinPassengers) {
-        val seat = zeppelin.getAvailableSeat() ?: return zeppelin
-
-        val passenger = Passenger(
-            name = "Alex",
-            lastName = "Ivanov",
-            document = DeputyID(
-                number = Random.nextInt(100000, 999999).toString()
-            ),
-            seat = seat
-        )
-        zeppelin.addPassenger(passenger)
-    }
     return zeppelin
 }
 
@@ -68,4 +55,33 @@ fun printPassengerInfo(aircraft: Aircraft, seat: Seat) {
         ?.getInfo()
         ?.let { println(it) }
         ?: println("there is no passenger")
+}
+
+fun fillAircraft(aircraft: Aircraft) {
+    val zeppelinPassengers = Random.nextInt(1, aircraft.capacity)
+    for (i in 0 until zeppelinPassengers) {
+        val seat = aircraft.getAvailableSeat() ?: return
+
+        val passenger = Passenger(
+            name = "Alex",
+            lastName = "Ivanov",
+            document = getRandomDocument(),
+            seat = seat
+        )
+        aircraft.addPassenger(passenger)
+    }
+}
+
+fun getRandomDocument(): Document {
+    when (Random.nextInt(0, 2)) {
+        0 -> ForeignPassport(
+            series = Random.nextInt(1000, 9999).toString(),
+            number = Random.nextInt(100_000, 999_999).toString()
+        )
+        else -> DeputyID(
+            number = Random.nextInt(100_000, 999_999).toString(),
+            issueDate = "01.01.1970"
+        )
+    }
+
 }
