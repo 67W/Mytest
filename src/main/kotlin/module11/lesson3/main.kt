953fe15996d78f0d89b07629ca9320c8190616c8
67W <hulimeno@yandex.ru>
2022-04-26 05:36:36 +0300

    Класс Enum обращения к классу вплоть до переменной и функции

--- src/main/kotlin/module11/lesson3/main.kt
+++ src/main/kotlin/module11/lesson3/main.kt
@@ -1,13 +1,8 @@
 package module11.lesson3
 
 fun main() {
-    val map = mutableMapOf(
-        1 to "1",
-        2 to "2",
-        3 to "3"
-    )
-
-    for ((key,value) in map) {
-        println("$key - $value")
-    }
+    Color.WHITE
+    println(Color.WHITE.hex)
+    Color.WHITE.draw()
+    Color.RED.draw()
 }
