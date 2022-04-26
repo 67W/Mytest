6cfe90ba3a16b9a62287cb9be706a365062130ea
67W <hulimeno@yandex.ru>
2022-04-26 05:19:40 +0300

    Классы данных, перечисления, изолированные классы   Интерпритация  деструктурирующего присваивания:     val person = Person("name", "last name")     val (name, lastName) = person

--- src/main/kotlin/module11/lesson3/main.kt
+++ src/main/kotlin/module11/lesson3/main.kt
@@ -2,5 +2,9 @@ package module11.lesson3
 
 fun main() {
     val person = Person("name", "last name")
-    val (name, lastName) = person
+
+//    val (name, lastName) = person
+
+    val name = person.component1()
+    val lastName = person.component2()
 }
