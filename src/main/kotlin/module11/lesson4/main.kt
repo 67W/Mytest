05072dafb27d5a8e6acb562119c585478f4271ce
67W <hulimeno@yandex.ru>
2022-04-27 17:52:16 +0300

    Обобщённые типы тип <T>

--- src/main/kotlin/module11/lesson4/main.kt
+++ src/main/kotlin/module11/lesson4/main.kt
@@ -1,8 +1,15 @@
 package module11.lesson4
 
+import module7.lesson1.Car
+
 fun main() {
     val list: List<String> = List(5) { "" }
     val intList = listOf(1, 2, 3, 4)
+
+    println(genericFun(5))
+    println(genericFun("hello"))
+    println(genericFun(Car()))
+    println(genericFun(null))
 }
 
 fun <T> genericFun(input: T): String {
