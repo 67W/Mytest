bf13c7790fb3e140eea17621f3a4b999e394381a
67W <hulimeno@yandex.ru>
2022-04-27 19:05:25 +0300

    Обобщённые типы В чем ошибка??

--- src/main/kotlin/module11/lesson4/main.kt
+++ src/main/kotlin/module11/lesson4/main.kt
@@ -1,9 +1,15 @@
 package module11.lesson4
 
+import module8.lesson3.Developer
+import module8.lesson3.Person
+
 fun main() {
     val first = Generic<Double>(5.0)
     val second = Generic<Int>(10)
     sum(first, second)
+
+    val developer = Developer("Tony", 40)
+    printDeveloper(Printer<Person>(), developer)
 }
 
 fun <T> genericFun(input: T): String {
@@ -14,4 +20,9 @@ fun sum(a: Generic<Number>, b: Generic<Number>): Int? {
     val first = a.getItem()?.toInt() ?: return null
     val second = b.getItem()?.toInt() ?: return null
     return first + second
+}
+
+fun printDeveloper(printer: Printer<Developer>, developer: Developer){
+//some logic
+    printer.print(developer)
 }
